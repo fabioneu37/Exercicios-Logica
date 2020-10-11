c648097e6eb8e76f687b649027e71e78892bef93
Fabio Souza <superneu@gmail.com>
2020-10-11 20:32:44 -0300

    treinamento OO

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+	private double loanLimit;
+
+	public BusinessAccount() {
+       super();
+	}
+
+	public BusinessAccount(Integer number, String holder, double balance, double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	public void loan(Double amount) {
+		if (amount <= loanLimit) {
+			balance += amount - 10.0;
+		}
+	}
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+	}
+}
---
+++ src/entities/OutsourcedEmployee.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+
+	private Double addtionalCharge;
+
+	public OutsourcedEmployee() {
+		super();
+	}
+
+	public OutsourcedEmployee(String name, Integer hours, double valuePerHour, double addtionalCharge) {
+		super(name, hours, valuePerHour);
+		this.addtionalCharge = addtionalCharge;
+	}
+
+	public double getAddtionalCharge() {
+		return addtionalCharge;
+	}
+
+	public void setAddtionalCharge(double addtionalCharge) {
+		this.addtionalCharge = addtionalCharge;
+	}
+
+	@Override
+	public double payment() {
+		return super.payment() + addtionalCharge * 1.1;
+	}
+
+}
