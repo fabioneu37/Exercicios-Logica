0958bab4364e783e890408264d342476ef18b9b7
Fabio de Souza Neu <superneu@gmail.com>
2020-11-22 16:39:36 -0300

    Possible move of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -16,4 +16,10 @@ public class Rook extends ChessPiece {
 		return "R";
 	}
 
+	@Override
+	public boolean[][] PossibleMoves() {
+		boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
